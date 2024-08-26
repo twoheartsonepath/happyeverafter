a7541052772b43faffb9e7b08b13ef4638b30f63
twoheartsonepath <whatsappbackup9677@gmail.com>
2024-08-26 21:47:50 +0530

    Update main.js
    increased timing for all idea elements to 0.7 to 1.2

--- main.js
+++ main.js
@@ -101,20 +101,20 @@ const animationTimeline = () => {
       },
       "+=0.7"
     )
-    .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
+    .from(".idea-1", 1.2, ideaTextTrans)
+    .to(".idea-1", 1.2, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 1.2, ideaTextTrans)
+    .to(".idea-2", 1.2, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 1.2, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff"
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-3", 1.2, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 1.2, ideaTextTrans)
+    .to(".idea-4", 1.2, ideaTextTransLeave, "+=1.5")
     .from(
       ".idea-5",
       1.4,
